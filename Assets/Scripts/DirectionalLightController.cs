8e3196a515d68477498e8d249e4b772c18a12d75
haneda-atsushi <hanecci@gmail.com>
2017-10-13 01:14:14 +0900

    Add light intensity animation to DirectionalLightController.cs

--- Assets/Scripts/DirectionalLightController.cs
+++ Assets/Scripts/DirectionalLightController.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class DirectionalLightController : MonoBehaviour
 {
-    public Vector3 m_FromEuler  = Vector3.zero;
-    public Vector3 m_ToEuler    = Vector3.zero;
+    public Vector3 m_FromEuler     = Vector3.zero;
+    public Vector3 m_ToEuler       = Vector3.zero;
+    public float   m_RotationSpeed = 0.1f;
 
-    public float   m_Speed      = 0.1f;
-    public bool    m_EnableLoop = true;
-
-    float          m_Time       = 0.0f;
+    float          m_Time          = 0.0f;
     Quaternion     m_QuatFrom;
     Quaternion     m_QuatTo;
+
+    public float   m_LightIntensitySpeed = 0.5f;
+    Light          m_Light;
+    float          m_LightIntensity;
 
     void Start()
     {
         m_QuatFrom = Quaternion.Euler( m_FromEuler );
         m_QuatTo   = Quaternion.Euler( m_ToEuler );
+
+        m_Light    = GetComponent<Light>();
+        Assert.IsNotNull( m_Light );
+
+        m_LightIntensity = m_Light.intensity;
     }
 
     void Update()
@@ -25,11 +33,10 @@ public class DirectionalLightController : MonoBehaviour
         m_Time += Time.deltaTime;
         this.transform.rotation = 
             Quaternion.Lerp( m_QuatFrom, m_QuatTo,
-                             m_Time * m_Speed );
+                             m_Time * m_RotationSpeed );
 
         float angle = Quaternion.Angle( this.transform.rotation, m_QuatTo );
-        if ( m_EnableLoop &&
-            ( angle == 0.0f ) )
+        if ( angle == 0.0f )
         {
             Quaternion tmp = m_QuatFrom;
             m_QuatFrom     = m_QuatTo;
@@ -37,5 +44,13 @@ public class DirectionalLightController : MonoBehaviour
 
             m_Time         = 0.0f;
         }
+
+        float deg = 180.0f * Time.realtimeSinceStartup * m_LightIntensitySpeed;
+        float rad = Mathf.Deg2Rad * deg;
+        float cos = Mathf.Cos( rad );
+
+        float light_offset    = 0.5f;
+        float light_intensity = ( m_LightIntensity - light_offset ) * ( 0.5f * cos + 0.5f ) + light_offset;
+        m_Light.intensity     = light_intensity;
     }
 }
